d304b2bca7fcbdaf5c6514882e2891e978ede360
AJEET KUMAR <cajeet@hotmail.com>
2022-05-09 11:39:09 +0530

    Update statistics.js
    min, max and average methods added

--- app/statistics.js
+++ app/statistics.js
@@ -1,5 +1,18 @@
 module.exports=function getStatistics(numbers){
     
     //implement the computation of statistics here
+    
+    let sortedNumbers = () => {
+        return numbers.sort(function(a,b) { return a - b;});
+    } 
+    min = () => {
+        return  sortedNumbers()[0]
+    }
+    max = () => {
+        return  sortedNumbers()[numbers.length-1];
+    }
+    average = () => {
+        return numbers.reduce(function(a,b) { return a+b ;})/numbers.length;
+    }
 }
 
